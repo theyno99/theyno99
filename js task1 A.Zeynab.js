288e617b90565cc085cd50548e7cd02b7d8590ba
Zeynab Addayeva <109240980+theyno99@users.noreply.github.com>
2023-07-14 03:38:57 +0400

    Add files via upload

---
+++ js task1 A.Zeynab.js
@@ -0,0 +1,121 @@
+
+let num1=12; //task1
+console.log(num1**2);
+num2=35; //task1.2
+console.log(num2**3);
+
+
+let //task2
+v=60 ;
+s=180;
+t=(s/v)
+console.log(t*60);// deqiqe sorusdugu ucun 60-a vururuq
+
+let //task3
+a=12
+b=121
+c=90;
+if (a>b)
+{if (b>c)
+    {console.log(a)}
+} 
+else if (b>c)
+    {console.log(b)} 
+else{console.log(c)};
+
+let z=152; //task4
+if (z%2==0){console.log("z cut ededdir")}
+else(console.log("z tek ededdir"));
+
+
+let day=6 ; // task5
+if (day==1){console.log("monday")} 
+else if (day==2) {console.log("tuesday") }
+else if (day==3) {console.log("wednesday")}
+else if (day==4) {console.log("thursday")} 
+else if (day==5) {console.log("friday")}
+else if (day==6) {console.log("saturday")}
+else if (day==7) {console.log("sunday")}
+
+let k=54; //task6
+m=k/10;
+f=Math.floor(m);//dersde kenara chixdighimin ferqindeyem amma daha parseInt ile chevire bilmedim
+l="";
+if(f==1){l="on "}
+else if (f==2){
+   (l="iyirmi ")
+}
+else if (f==3){
+    (l="otuz ")
+}
+else if (f==4){
+    (l="qirx ")
+}
+else if (f==5){
+    (l="elli ")
+}
+else if (f==6){
+    (l="altmish ")
+}
+else if (f==7){
+    (l="yetmish ")
+}
+else if (f==8){
+    (l="seksen ")
+}
+else if (f==9){
+    (l="doxsan ")
+};
+
+if (k % 10==1){
+    console.log(l+"bir")
+}
+else if (k % 10==2){
+    console.log(l+"iki")
+
+}
+else if (k % 10==3){
+    console.log(l+"uc")
+
+}
+else if (k % 10==4){
+    console.log(l+"dord")
+
+}
+else if (k % 10==5){
+    console.log(l+"bes")
+
+}
+else if (k % 10==6){
+    console.log(l+"alti")
+
+}
+else if (k % 10==7){
+    console.log(l+"yeddi")
+
+}
+else if (k % 10==8){
+    console.log(l+"sekkiz")
+
+}
+else if (k % 10==9){
+    console.log(l+"doqquz")
+};
+
+
+let //task7
+g=76;
+h=g/10
+if(h<1){console.log("birreqemli eded")}
+else if(h<=9){
+    console.log("ikireqemli eded")}
+else if(h<=99){
+    console.log("ucreqemli eded")}
+else if(h>100){
+    console.log("dord ve ya cox reqemli eded")};
+    //////////////////////////////////////
+    /// 
+let i=0;
+for (let i=0; i<10 ; i++ ){
+    console.log(i)   
+    }
